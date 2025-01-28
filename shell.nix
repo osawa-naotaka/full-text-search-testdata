dfa525c405490852d64b03112a42380453918e32
osawa naotaka <ohsawa.naotaka@gmail.com>
2025-01-28 20:23:06 +0900

    update shell.nix

--- shell.nix
+++ shell.nix
@@ -5,19 +5,13 @@ in pkgs.mkShell rec {
 
   buildInputs = with pkgs; [
     git
+    pyenv
     python312
     python312Packages.requests
-    python312Packages.langdetect
     python312Packages.mecab-python3
     python312Packages.unidic-lite
     python312Packages.mwparserfromhell
-    python312Packages.tqdm
-    python312Packages.fugashi
-    python312Packages.unidic-lite
     python312Packages.beautifulsoup4
     nodejs_23
-    php84
-    php84Packages.composer
-    pandoc
   ];
 }
